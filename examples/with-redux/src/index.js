b0d1686482f8324ec24dd44a52cdbac8fef01fc7
Takashi Ueno <pakkunflower00@gmail.com>
2018-04-13 01:13:19 +0900

    Update examples

--- examples/with-redux/src/index.js
+++ examples/with-redux/src/index.js
@@ -10,38 +10,51 @@ import NotFoundPage from "./pages/NotFoundPage";
 import 'bootstrap/dist/css/bootstrap.css';
 import "font-awesome/css/font-awesome.min.css";
 import "nprogress/nprogress.css";
-import { Subject } from "rxjs";
+import {Subject} from "rxjs";
 
 // Set RxJS
 const stream = new Subject();
 
-NextProps.streamer = (load, setInitialProps) => {
-    stream
-        .switchMap(load)
-        .subscribe(setInitialProps);
-};
+class NextPropsWrapper extends React.Component {
+    streamer(load, setInitialProps) {
+        stream
+            .switchMap(load)
+            .subscribe(setInitialProps);
+    }
 
-NextProps.fireStreamer = () => {
-    stream.next();
-};
+    fireStreamer() {
+        stream.next();
+    }
 
-NextProps.firstRender = () => {
-  return (
-      <div className="text-center" style={{margin: "100px 0"}}>
-          <i className="fa fa-cog fa-spin fa-5x fa-fw"/>
-      </div>
-  );
-};
+    firstRender() {
+        return (
+            <div className="text-center" style={{margin: "100px 0"}}>
+                <i className="fa fa-cog fa-spin fa-5x fa-fw"/>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <NextProps
+                streamer={this.streamer.bind(this)}
+                fireStreamer={this.fireStreamer.bind(this)}
+                firstRender={this.firstRender.bind(this)}
+                {...this.props}
+            />
+        );
+    }
+}
 
 function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route path="/" exact render={(props) => (<NextProps {...props} component={IndexPage}/>)}/>
-                <Route path="/news/:page?" exact render={(props) => (<NextProps {...props} component={NewsPage}/>)}/>
-                <Route path="/item/:itemId" exact render={(props) => (<NextProps {...props} component={ItemPage}/>)}/>
-                <Route path="/user/:userId" exact render={(props) => (<NextProps {...props} component={UserPage}/>)}/>
-                <Route component={NotFoundPage}/>
+                <Route path="/" exact render={(props) => (<NextPropsWrapper {...props} component={IndexPage}/>)}/>
+                <Route path="/news/:page?" exact render={(props) => (<NextPropsWrapper {...props} component={NewsPage}/>)}/>
+                <Route path="/item/:itemId" exact render={(props) => (<NextPropsWrapper {...props} component={ItemPage}/>)}/>
+                <Route path="/user/:userId" exact render={(props) => (<NextPropsWrapper {...props} component={UserPage}/>)}/>
+                <Route render={(props) => (<NextPropsWrapper {...props} component={NotFoundPage}/>)}/>
             </Switch>
         </HashRouter>
     );
